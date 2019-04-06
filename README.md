14db6d110788fd756b42e42059593cf00aca7a94
Ash <ashley@juice49.com>
2019-04-06 18:34:50 +0100

    :rocket:

---
+++ README.md
@@ -0,0 +1,5 @@
+# gatsby-transformer-ndjson
+
+A Gatsby transformer that creates nodes from each entry in ndjson files.
+
+Not published to npm yet because I'm not sure what I'm doing!
---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const fun = require('funstream')
+
+exports.onCreateNode = async ({ node, actions }) => {
+  if (node.extension !== `ndjson`) {
+    return
+  }
+
+  await fun(fs.createReadStream(node.absolutePath))
+    .ndjson()
+    .forEach(item => actions.createNode({
+      ...item,
+      internal: {
+        type: node.sourceInstanceName
+      }
+    }))
+}
